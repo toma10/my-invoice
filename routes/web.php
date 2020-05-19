9e56ffdbf50ff9ac0101f13f1cb354a32233844a
Tomáš Máčala <tomas.macala@seznam.cz>
2020-05-19 13:58:23 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -40,12 +40,7 @@ Route::view('admin/invoices', 'admin.invoices.index')->name('admin.invoices.inde
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', [InvoicesController::class, 'index'])->name('invoices.index');
-    Route::get('invoices/create', [InvoicesController::class, 'create'])->name('invoices.create');
-    Route::post('invoices', [InvoicesController::class, 'store'])->name('invoices.store');
-    Route::get('invoices/{invoice}', [InvoicesController::class, 'show'])->name('invoices.show');
-    Route::get('invoices/{invoice}/edit', [InvoicesController::class, 'edit'])->name('invoices.edit');
-    Route::put('invoices/{invoice}', [InvoicesController::class, 'update'])->name('invoices.update');
-    Route::delete('invoices/{invoice}', [InvoicesController::class, 'destroy'])->name('invoices.destroy');
+    Route::resource('invoices', InvoicesController::class)->except('index');
 
     Route::post('invoices/{invoice}/download', DownloadInvoiceController::class)->name('invoices.download');
 });
