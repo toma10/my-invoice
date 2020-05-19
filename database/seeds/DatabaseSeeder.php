b291a2a413556b85646a3d0907bcc7da6dafbddd
Tomáš Máčala <tomas.macala@seznam.cz>
2020-05-19 12:50:20 +0200

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,8 @@ use App\Department;
 use App\Invoice;
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\Testing\File;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,14 +24,24 @@ class DatabaseSeeder extends Seeder
         $departmentB = factory(Department::class)->create(['name' => 'Department B']);
         $departmentC = factory(Department::class)->create(['name' => 'Department C']);
 
+        $pdfA = File::create('invoice-a.pdf');
+        $pdfPathA = Storage::putFile('invoices', $pdfA);
+
         factory(Invoice::class)->create([
             'user_id' => $johnDoe,
             'department_id' => $departmentA,
+            'pdf_file_path' => $pdfPathA,
+            'pdf_file_filename' => 'invoice-a.pdf',
         ]);
+
+        $pdfB = File::create('invoice-b.pdf');
+        $pdfPathB = Storage::putFile('invoices', $pdfB);
 
         factory(Invoice::class)->create([
             'user_id' => $janeDoe,
             'department_id' => $departmentC,
+            'pdf_file_path' => $pdfPathB,
+            'pdf_file_filename' => 'invoice-b.pdf',
         ]);
     }
 }
