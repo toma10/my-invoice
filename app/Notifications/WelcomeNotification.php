19770344adadaf5fbd7c8cb5119d036bb7968f28
Tomáš Máčala <tomas.macala@seznam.cz>
2020-05-26 15:12:31 +0200

    Update WelcomeNotification.php

--- app/Notifications/WelcomeNotification.php
+++ app/Notifications/WelcomeNotification.php
@@ -12,9 +12,10 @@ class WelcomeNotification extends Notification
     public function toMail($notifiable): MailMessage
     {
         return (new MailMessage())
+            ->subject(sprintf('Welcome to %s', config('app.name')))
             ->line(
                 sprintf(
-                    "You 've been invited to use %s. You can setup your account using button below.",
+                    "You've been invited to use %s. You can setup your account using button below.",
                     config('app.name')
                 )
             )
