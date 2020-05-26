0ec866d71095cde4e6deb35203200dcce1b5b0cc
Tomáš Máčala <tomas.macala@seznam.cz>
2020-05-26 15:12:21 +0200

    Update panel.blade.php

--- resources/views/components/auth/panel.blade.php
+++ resources/views/components/auth/panel.blade.php
@@ -1,5 +1,6 @@
 <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
   <div class="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+    @include('layouts.partials._flash')
     {{ $slot }}
   </div>
 </div>
