98bc89eb6986660d400d88332993a96f2b7328a8
Tomáš Máčala <tomas.macala@seznam.cz>
2020-05-26 15:12:43 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,12 +6,26 @@
 <x-auth.panel>
   <x-form :action="route('login')">
     <div class="space-y-6">
-      <x-text-field type="email" name="email" label="Email" required autofocus />
-      <x-text-field type="password" name="password" label="Password" required />
+      <x-text-field
+        type="email"
+        name="email"
+        label="Email"
+        required
+        autofocus
+      />
+
+      <x-text-field
+        type="password"
+        name="password"
+        label="Password"
+        required
+      />
+
       <div class="flex items-center justify-between">
         <x-checkbox-field name="remember" label="Remember me" />
         <x-link :href="route('password.request')">Forgot your password?</x-link>
       </div>
+
       <x-button full-width>Sign In</x-button>
     </div>
   </x-form>
