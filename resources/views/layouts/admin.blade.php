c93958d55d5fa330b3d8de4dac5132c4cca10478
Tomáš Máčala <tomas.macala@seznam.cz>
2020-05-26 15:12:14 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,7 +9,6 @@
     <x-container>
       <div class="py-10">
         @include('layouts.partials._flash')
-
         <div class="lg:grid lg:grid-cols-12 lg:col-gap-12 space-y-8 lg:space-y-0">
           <div class="lg:col-span-3 xl:col-span-2">
             @include('layouts.partials.admin._nav')
