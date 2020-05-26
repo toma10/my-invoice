276fbae2db47dd78976c02f75371abc675000e72
Tomáš Máčala <tomas.macala@seznam.cz>
2020-05-26 11:21:45 +0200

    Update title.blade.php

--- resources/views/components/auth/title.blade.php
+++ resources/views/components/auth/title.blade.php
@@ -1,5 +1,5 @@
 <div class="sm:mx-auto sm:w-full sm:max-w-md">
-  <h2 class="mt-3 text-3xl font-extrabold leading-9 text-center text-gray-900">
+  <h2 class="mt-3 text-3xl font-semibold leading-9 text-center text-gray-900">
     {{ $slot }}
   </h2>
 </div>
